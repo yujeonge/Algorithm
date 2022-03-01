f1dbb7cbc6bb0717b33bd504124beb78c4a3e5c7
jeong746 <jeong746@naver.com>
2022-03-01 18:51:32 +0900

    Create B1000_AplusB.java

---
+++ BOJ_git/B1000_AplusB.java
@@ -0,0 +1,13 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B1000_AplusB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		int j = sc.nextInt();
+		System.out.println(i+j);
+	}
+}
+
