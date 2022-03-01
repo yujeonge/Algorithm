6649ceb0802d7f2db41f9d622314bdfec7a38aa1
jeong746 <jeong746@naver.com>
2022-03-01 19:07:09 +0900

    Create

---
+++ BOJ_git/src/B1110_더하기_사이클.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B1110_더하기_사이클 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		boolean isNotSame = true;
+		int cycle = 0;
+		int C = N;
+		do {
+			C = (C % 10 * 10) + ((C / 10 + C % 10) % 10); // 60+8
+			cycle++;
+			if (C == N) {
+				isNotSame = false;
+			}
+		} while (isNotSame);
+
+		System.out.println(cycle);
+	}
+}
