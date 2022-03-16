cf02d6039e48b665c6efe6cc1e2b59cc696cac07
jeong746 <jeong746@naver.com>
2022-03-16 18:58:18 +0900

    Create

---
+++ BOJ_git/src/B1676_팩토리얼_0의_개수.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Scanner;
+
+public class B1676_팩토리얼_0의_개수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		int two = 0;
+		int five = 0;
+		for (int i = 1; i <= N; i++) {
+			int n = i;
+
+			while (n % 2 == 0) { // 2 개수 세기
+				two++;
+				n /= 2;
+			}
+
+			while (n % 5 == 0) { // 5 개수 세기
+				five++;
+				n /= 5;
+			}
+		}
+
+		// 2와 5로 만들 수 있는 10의 개수의 최솟값
+		System.out.println(five < two ? five : two);
+	}
+
+}
