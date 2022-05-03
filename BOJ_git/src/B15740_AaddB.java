f8403d6d2e26795cf48c1a8b54bbebc826db9060
jeong746 <jeong746@naver.com>
2022-05-03 13:42:33 +0900

    Create

---
+++ BOJ_git/src/B15740_AaddB.java
@@ -0,0 +1,13 @@
+
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B15740_AaddB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		System.out.println(a.add(b));
+	}
+}
