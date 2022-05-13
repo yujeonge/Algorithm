a0b04933a51f57f669efdea7db0f45d1c0391184
jeong746 <jeong746@naver.com>
2022-05-13 12:41:14 +0900

    Create

---
+++ BOJ_git/src/B11382_꼬마_정민.java
@@ -0,0 +1,10 @@
+
+
+import java.util.Scanner;
+
+public class B11382_꼬마_정민 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+	}
+}
