9b71a6bb53210a92d1a803da88caf2a2e87890e6
jeong746 <jeong746@naver.com>
2022-05-16 13:24:58 +0900

    Create

---
+++ BOJ_git/src/B2420_사파리_월드.java
@@ -0,0 +1,10 @@
+
+
+import java.util.Scanner;
+
+public class B2420_사파리_월드 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(Math.abs(sc.nextLong()-sc.nextLong()));
+	}
+}
