82207e40f040c2ab40fd3627050b4ba012d50ae0
jeong746 <jeong746@naver.com>
2022-05-17 13:35:00 +0900

    Create

--- BOJ_git/src/B1669_멍멍이_쓰다듬기.java
+++ BOJ_git/src/B1669_멍멍이_쓰다듬기.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,24 +7,28 @@ public class B1669_멍멍이_쓰다듬기 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken()); //원숭이 키
+		int y = Integer.parseInt(st.nextToken()); //멍멍이 키
 		int t = y - x; // 늘려야하는 키
-		long n = 0;
-		if (t == 0) {
+		
+		if (t == 0) {  //키 차이가 안나면
 			System.out.println(0);
 			return;
 		}
-		while (n * n < t) {
+		
+		long n = 0; //t와 일 때 사용할 수 있는 가장 큰 제곱근 수
+
+		while (n * n < t) { //n구하기
 			n++;
 		}
-		if (n * n != t)
-			n --;
-		long ans = 2 * n - 1;
+		
+		if (n * n != t) n--;
+		
+		long ans = 2 * n - 1; //키 크는 과정이 걸리는 일 수
 		t -= n * n;
 
-		while (t > 0) {
-			t -= Math.min(n, t);
+		while (t > 0) { //키 차이가 안날때까지 
+			t -= Math.min(n, t); //남은 키 차이 갱신
 			ans++;
 		}
 
