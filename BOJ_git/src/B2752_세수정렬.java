87b643efa72a71b9d0b5b5df99df41b71a7c3b4e
yujeonge <jeong000746@gmail.com>
2022-05-28 12:32:06 +0900

    Create

---
+++ BOJ_git/src/B2752_세수정렬.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B2752_세수정렬 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[3];
+		for (int i = 0; i < 3; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+
+		for (int i = 0; i < 3; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
