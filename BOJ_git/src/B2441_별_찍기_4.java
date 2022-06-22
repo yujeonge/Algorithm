5ac2e5c567c6c01cd5a2c6684254b93e7765fb6a
yujeonge <jeong000746@gmail.com>
2022-06-22 11:54:18 +0900

    Create

---
+++ BOJ_git/src/B2441_별_찍기_4.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class B2441_별_찍기_4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = n - i; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+	}
+}
