ac3fe107e46c64d07393fdd5af7bc64181fbca97
yujeonge <jeong000746@gmail.com>
2022-06-25 11:10:02 +0900

    Create

---
+++ BOJ_git/src/B25083_새싹.java
@@ -0,0 +1,14 @@
+
+
+public class B25083_새싹 {
+	public static void main(String[] args) {
+		String s = "         ,r'\"7\n" + 	// \", \n 이 제어문자다.
+				   "r`-_   ,'  ,/\n" + 		// \n 이 제어문자다.
+				   " \\. \". L_r'\n" + 		// \\, \", \n 이 제어문자다.
+				   "   `~\\/\n" + 			// \\, \n 이 제어문자다.
+				   "      |\n" + 			// \n 이 제어문자다.
+				   "      |";
+ 
+		System.out.print(s);
+	}
+}
