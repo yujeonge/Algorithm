9f8458d127d5a1eb533b097185061dec97e1c814
yujeonge <jeong000746@gmail.com>
2022-07-03 21:14:18 +0900

    Create

---
+++ BOJ_git/src/B10807_개수_세기.java
@@ -0,0 +1,19 @@
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class B10807_개수_세기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int tc = sc.nextInt();
+		for (int i = 0; i < tc; i++) {
+			int num = sc.nextInt();
+			map.put(num, map.containsKey(num) ? map.get(num) + 1 : 1);
+		}
+		int v = sc.nextInt();
+		int ans = map.containsKey(v) ? map.get(v) : 0;
+		System.out.println(ans);
+	}
+}
