4b0c2f63116eab65b178e51a5df6cc29036566f5
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-07-12 22:15:50 +0900

    Create

---
+++ BOJ_git/src/B15680_연세대학교.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class B15680_연세대학교 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if(a==0) {
+			System.out.println("YONSEI");
+		}else {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}
