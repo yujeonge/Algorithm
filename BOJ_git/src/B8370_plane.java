d4709abe14cc6ad982e7450bb89bac7805c7d4cf
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-08-04 21:09:47 +0900

    Create

---
+++ BOJ_git/src/B8370_plane.java
@@ -0,0 +1,10 @@
+package algo2;
+
+import java.util.Scanner;
+
+public class B8370_plane {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println((sc.nextInt() * sc.nextInt()) + (sc.nextInt() * sc.nextInt()));
+	}
+}
