a808e90a2b470f83a070ffd7158ca5358cc840b7
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-08-22 10:14:52 +0900

    Create

---
+++ BOJ_git/src/B15700_타일_채우기_4.java
@@ -0,0 +1,11 @@
+package algo2;
+
+import java.util.Scanner;
+
+public class B15700_타일_채우기_4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long x = sc.nextLong(), y = sc.nextLong();
+		System.out.println(x * y / 2);
+	}
+}
