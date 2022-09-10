7a226acf832585ac787ad7ad822cfc60dd43a92e
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-09-10 22:13:11 +0900

    Create

---
+++ BOJ_git/src/B2455_지능형_기차.java
@@ -0,0 +1,25 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B2455_지능형_기차 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int max = 0;
+		int now = 0;
+		for (int i = 0; i < 4; i++) {
+			st = new StringTokenizer(br.readLine());
+			int bye = Integer.valueOf(st.nextToken());
+			int hello = Integer.valueOf(st.nextToken());
+
+			now = now - bye + hello;
+			max = Math.max(max, now);
+		}
+		System.out.println(max);
+	}
+}
