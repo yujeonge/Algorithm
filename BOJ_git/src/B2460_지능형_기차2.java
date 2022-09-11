b8018d03fa7b4ce5632ac98edc58bea63713a1b2
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-09-11 19:07:07 +0900

    Create

---
+++ BOJ_git/src/B2460_지능형_기차2.java
@@ -0,0 +1,20 @@
+package algo;
+
+import java.util.Scanner;
+
+public class B2460_지능형_기차2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int human = 0;
+		int max = 0;
+		for (int i = 0; i < 10; i++) {
+			int out = sc.nextInt();
+			int in = sc.nextInt();
+			human -= out;
+			human += in;
+			if (human > max)
+				max = human;
+		}
+		System.out.println(max);
+	}
+}
