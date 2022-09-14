deee91c4c5077c016beaae0c84c17f7bc04872aa
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-09-14 09:29:33 +0900

    Create

---
+++ BOJ_git/src/B13277_큰_수_곱셈.java
@@ -0,0 +1,13 @@
+package algo;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B13277_Å«_¼ö_°ö¼À {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		System.out.println(a.multiply(b));
+	}
+}
