a95c4ce821e0ba0348b73f0436b8badd361271bc
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-09-15 13:28:25 +0900

    Create

---
+++ BOJ_git/src/B16394_홍익대학교.java
@@ -0,0 +1,11 @@
+package algo;
+
+import java.util.Scanner;
+
+public class B16394_홍익대학교 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n - 1946);
+	}
+}
