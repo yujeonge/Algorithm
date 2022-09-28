61ac7d351ee25095485489f6f88f8b126fe6cd99
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-09-28 09:36:14 +0900

    Create

---
+++ BOJ_git/src/B16430_제리와_톰.java
@@ -0,0 +1,13 @@
+package algo;
+
+import java.util.Scanner;
+
+public class B16430_Á¦¸®¿Í_Åè {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(b - a + " " + b);
+	}
+}
