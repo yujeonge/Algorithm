04bee3c1a1ad589668973bfc73fda9bf35501ef5
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-10-05 09:03:41 +0900

    Create

---
+++ BOJ_git/src/B18301_Rate.java
@@ -0,0 +1,13 @@
+package algo;
+
+import java.util.Scanner;
+
+public class B18301_Rate {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		System.out.print((n1 + 1) * (n2 + 1) / (n3 + 1) - 1);
+	}
+}
