caf227315937908f9489f9cf06ea7f84bfcc2b3e
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-10-09 12:53:38 +0900

    Create

---
+++ BOJ_git/src/B3460_이진수.java
@@ -0,0 +1,29 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B3460_ÀÌÁø¼ö {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			int count = 0;
+			int N = Integer.parseInt(br.readLine());
+			while (true) {
+				if (N == 1) {
+					System.out.println(count);
+					break;
+				}
+				if (N % 2 == 1) {
+					System.out.print(count + " ");
+				}
+				count++;
+				N /= 2;
+			}
+		}
+	}
+
+}
