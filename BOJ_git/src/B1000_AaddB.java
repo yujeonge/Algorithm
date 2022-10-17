93ca8f4b60e4edb12b524d1efc42c10d52f7057b
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-10-17 23:54:29 +0900

    create

--- BOJ_git/src/B1000_AaddB.java
+++ BOJ_git/src/B1000_AaddB.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class B1000_AaddB {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc  = new Scanner(System.in);
 		int i = sc.nextInt();
 		int j = sc.nextInt();
 		System.out.println(i+j);
