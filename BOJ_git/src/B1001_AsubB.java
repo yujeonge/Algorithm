71d37edf81eea6dfde4ec4fdfb53d2b3e71c34c2
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-11-04 21:58:13 +0900

    Create

--- BOJ_git/src/B1001_AsubB.java
+++ BOJ_git/src/B1001_AsubB.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class B1001_AsubB {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc =  new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
         System.out.println(A-B);
