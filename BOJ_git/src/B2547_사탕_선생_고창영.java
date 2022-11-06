8fec895632a34ff624061b798d3843a94b5e5451
yujeonge <98456240+yujeonge@users.noreply.github.com>
2022-11-06 15:50:21 +0900

    Create

---
+++ BOJ_git/src/B2547_사탕_선생_고창영.java
@@ -0,0 +1,27 @@
+package algo;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B2547_사탕_선생_고창영 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			BigInteger sum = new BigInteger("0");
+
+			for (int j = 0; j < n; j++) {
+				sum = sum.add(scanner.nextBigInteger());
+			}
+
+			String reminder = String.valueOf(sum.remainder(BigInteger.valueOf(n)));
+			if (reminder.equals("0"))
+				System.out.println("YES");
+			else
+				System.out.println("NO");
+		}
+	}
+}
