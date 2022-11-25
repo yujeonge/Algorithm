bc8769c7d6913c0a0792e1ce4cb0b14f91f71e8d
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2022-11-25 17:55:10 +0900

    Create

---
+++ BOJ_git/src/B24082_Cube.java
@@ -0,0 +1,12 @@
+package algo;
+
+import java.util.Scanner;
+
+public class B24082_Cube {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		System.out.println((int) Math.pow(n, 3));
+	}
+}
