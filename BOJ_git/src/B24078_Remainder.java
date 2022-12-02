1740452e05c7777cbdcc9c389578a9adcebcdd0c
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2022-12-02 23:11:10 +0900

    Create

---
+++ BOJ_git/src/B24078_Remainder.java
@@ -0,0 +1,10 @@
+package algo;
+
+import java.util.Scanner;
+
+public class B24078_Remainder {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() % 21);
+	}
+}
