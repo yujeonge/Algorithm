2b472661ffa6b89c7e066d5f7d4d1df17ef8e5ef
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2022-12-05 21:59:20 +0900

    Create

---
+++ BOJ_git/src/B26209_Intercepting_Information.java
@@ -0,0 +1,17 @@
+package algo;
+
+import java.util.Scanner;
+
+public class B26209_Intercepting_Information {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 8; i++) {
+			int n = sc.nextInt();
+			if (n > 1) {
+				System.out.println('F');
+				return;
+			}
+		}
+		System.out.println('S');
+	}
+}
