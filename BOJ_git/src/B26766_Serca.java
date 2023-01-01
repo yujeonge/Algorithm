afdce0dd59aa25e6b5251107c473d1f465c51c40
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-01-01 16:19:29 +0900

    Create

---
+++ BOJ_git/src/B26766_Serca.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B26766_Serca {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			sb.append(" @@@   @@@ ").append('\n');
+			sb.append("@   @ @   @").append('\n');
+			sb.append("@    @    @").append('\n');
+			sb.append("@         @").append('\n');
+			sb.append(" @       @ ").append('\n');
+			sb.append("  @     @  ").append('\n');
+			sb.append("   @   @   ").append('\n');
+			sb.append("    @ @    ").append('\n');
+			sb.append("     @     ").append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
