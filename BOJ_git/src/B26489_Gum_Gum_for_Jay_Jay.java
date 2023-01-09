823c4fda5aba0107a4a7da42b2027a5df6bc84e6
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-01-09 19:21:46 +0900

    Create

---
+++ BOJ_git/src/B26489_Gum_Gum_for_Jay_Jay.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B264 package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B26489_Gum_Gum_for_Jay_Jay {
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int cnt = 0;
+      
+      while (true) {
+         String str = br.readLine();
+         if (str == null)
+            break;
+         cnt++;
+      }
+      System.out.println(cnt);
+   }
+}
