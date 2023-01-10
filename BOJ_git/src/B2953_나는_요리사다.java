f242ac26d818759962a7596f013d18b6a89a35f1
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-01-10 14:49:01 +0900

    Create

---
+++ BOJ_git/src/B2953_나는_요리사다.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B2953_나는_요리사다 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ans = 0;
+		int score = 0;
+
+		for (int i = 1; i < 6; i++) {
+			int temp = 0;
+			for (int j = 0; j < 4; j++) {
+				temp += sc.nextInt();
+			}
+			score = Math.max(score, temp);
+
+			if (score == temp) {
+				ans = i;
+			}
+		}
+
+		System.out.println(ans + " " + score);
+	}
+}
