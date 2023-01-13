148bf71ab3675f5eeb7fcf97aa0269748922c86f
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-01-13 19:58:18 +0900

    Create

--- BOJ_git/src/B13458_시험_감독.java
+++ BOJ_git/src/B13458_시험_감독.java
@@ -2,9 +2,6 @@
 
 import java.util.Scanner;
 
-/*첫째 줄에 시험장의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다.
-둘째 줄에는 각 시험장에 있는 응시자의 수 Ai (1 ≤ Ai ≤ 1,000,000)가 주어진다.
-->최악의 경우 범위 조심*/
 public class B13458_시험_감독 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
