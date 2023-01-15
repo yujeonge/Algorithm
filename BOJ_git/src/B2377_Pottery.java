4020d2da08243c84cf1920136adbe2b6781ef195
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-01-15 15:19:37 +0900

    Create

---
+++ BOJ_git/src/B2377_Pottery.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+public class B2377_Pottery {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(" _________").append('\n');
+		sb.append(" \\_     _/").append('\n');
+		sb.append("   \\   /").append('\n');
+		sb.append("    | |").append('\n');
+		sb.append("   /   \\").append('\n');
+		sb.append("  /     \\").append('\n');
+		sb.append(" |       |").append('\n');
+		sb.append("/---------\\").append('\n');
+		sb.append("| \\-/ \\-/ |").append('\n');
+		sb.append("\\---------/").append('\n');
+		sb.append(" \\_______/").append('\n');
+		
+		System.out.println(sb);
+	}
+}
