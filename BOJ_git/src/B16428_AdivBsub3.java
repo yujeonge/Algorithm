89ca3090d4c666842a53797407dd3dcbce455b9c
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-01-25 18:29:41 +0900

    Create

---
+++ BOJ_git/src/B16428_AdivBsub3.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B16428_AdivBsub3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		if (b.compareTo(BigInteger.ZERO) == -1) {
+			b = b.abs();
+			System.out.println("-" + a.divide(b));
+			System.out.println(a.mod(b));
+		} else {
+			System.out.println(a.divide(b));
+			System.out.println(a.mod(b));
+		}
+	}
+}
