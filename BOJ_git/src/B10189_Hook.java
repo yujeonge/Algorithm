bfcaa3a249927334f53d1c76358b6824558fb162
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-01-28 20:44:12 +0900

    Create

---
+++ BOJ_git/src/B10189_Hook.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+public class B10189_Hook {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("#  # #### #### #  #").append('\n');
+		sb.append("#### #  # #  # # #").append('\n');
+		sb.append("#### #  # #  # # #").append('\n');
+		sb.append("#  # #### #### #  #").append('\n');
+		System.out.println(sb);
+	}
+}
