1d036970fdc393bf45180c77ccf04b3ea98d3424
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-02-11 23:17:44 +0900

    Create

--- BOJ_git/src/B26545_Mathematics.java
+++ BOJ_git/src/B26545_Mathematics.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class B26545_Mathematics {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int n =  sc.nextInt();
 		int ans = 0;
 
 		for (int i = 0; i < n; i++) {
