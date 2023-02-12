b890c769da532365c3b6a3e07090d8caa731c644
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-02-12 23:23:32 +0900

    Create

---
+++ BOJ_git/src/B27327_Hour.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B27327_Hour {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() * 24);
+	}
+}
