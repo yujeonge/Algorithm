e8bcabfdfb1da30396310e705609c3436ec3fdb4
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-02-17 14:34:46 +0900

    Create

---
+++ BOJ_git/src/B144337_Helicopter.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+public class B144337_Helicopter {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("    ===================").append('\n');
+		sb.append("            ____||___").append('\n');
+		sb.append("  \\ /      /       []\\").append('\n');
+		sb.append("   X=======           \\__").append('\n');
+		sb.append("  / \\      \\____________|").append('\n');
+		sb.append("              ||  ||").append('\n');
+		sb.append("           \\-----------/").append('\n');
+		System.out.println(sb);
+	}
+}
