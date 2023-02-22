3470489ac5ca17f600794cb0e0f1e97093a919ad
KimYouJeong <98456240+yujeonge@users.noreply.github.com>
2023-02-22 15:11:14 +0900

    Create

---
+++ BOJ_git/src/B27331_2자리_정수.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B27331_2자리_정수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() * 10 + sc.nextInt());
+	}
+}
